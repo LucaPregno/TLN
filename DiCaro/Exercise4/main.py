873842d1f9f843e6c7ce0cb6284fe83a8372c4c9
Gremmus <luca.prs8@gmail.com>
2020-11-06 02:24:12 +0100

    fix cleaning and exercise 4

--- DiCaro/Exercise4/main.py
+++ DiCaro/Exercise4/main.py
@@ -1,22 +1,21 @@
 import os
 import re
 from collections import Counter
-from DiCaro.Utility import parser
+from DiCaro.Utility import parser, plot
 
 INPUT_PATH = os.path.abspath('../DiCaro/Exercise4/resources/input.txt')
-CLUSTER_STEP = 5
+CLUSTER_STEP = 4
 
 
 def main():
-    sentences = process_file(INPUT_PATH)
-    cluster_sentences(sentences)
+    sentence_table = process_file(INPUT_PATH)
+    plot.print_table(sentence_table)
+    # sentence_table = cluster_sentences(sentence_table)
 
 
-def process_file(path: str):
+def process_file(path: str) -> list:
     file = open(path, "r")
     sentences = re.split('[.!?]', file.read())
-    for s in sentences:
-        print(s)
     file.close()
     processed_sentence = []
     for sentence in sentences:
@@ -25,20 +24,15 @@ def process_file(path: str):
     return processed_sentence
 
 
-def cluster_sentences(sentences):
-    step = round(len(sentences) * CLUSTER_STEP/100)
-    clustered_sentences = []
+def cluster_sentences(sentences: list, step: int = CLUSTER_STEP) -> list:
+    # step = round(len(sentences) * CLUSTER_STEP/100)
+    table_sentence = []
     counter = Counter()
     for i in range(len(sentences)):
         counter = counter + sentences[i]
         if i % step == 0 or i == len(sentences):
-            clustered_sentences.append((counter.copy(), i))
+            table_sentence.append(counter.copy())
             counter.clear()
     print("CLUSTERED SENTENCES")
-    print(list(map(print, clustered_sentences)))
-    # print(clustered_sentences, "\n length:", len(clustered_sentences))
-
-
-
-
-
+    print(list(map(print, table_sentence)))
+    return table_sentence
